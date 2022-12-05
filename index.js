dfdcbbe9bff038b199c58e77372970c93cd5a8c8
Giovane Rossatto <giovanerjg@gmail.com>
2022-12-05 03:07:36 -0300

    post nao funcionou

--- index.js
+++ index.js
@@ -4,17 +4,26 @@ const port = 3000;
 const fs = require("fs");
 const bodyParser = require("body-parser");
 const { text } = require("body-parser");
+var userList = []
 
 app.set('views', './views');
 app.set("view engine", "pug");
 
-app. get ("/users-form", (reg, res) => {
-    fs.readFile(__dirname + "/form.html", "utf8", (err, text) =>
-    {
-    res.send(text);
-    });
-    });
+// app.get("/users-form", (reg, res) => {
+//     fs.readFile(__dirname + "/form.html", "utf8", (err, text) =>
+//     {
+//     res.send(text);
+//     });
+//     });
 
+app.get('/pug', (req, res) => {
+    res.render('index')
+})
+
+app.post('/submit', (req, res) => {
+    userList.push(req.body)
+    console.log(userList);
+})
 
 
 app.listen(port, () => {
